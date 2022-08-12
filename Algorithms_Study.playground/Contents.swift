2350cbc47bf32535b2661c7b95740e2fb9b859a6
Jason Stelzel <laptop@epigraf.com>
2022-08-11 18:26:46 -0700

    Basic Fibonacci Series Solver with Memoization Solution, using inout

--- Algorithms_Study.playground/Contents.swift
+++ Algorithms_Study.playground/Contents.swift
@@ -36,3 +36,28 @@
 
 
 
+// ---> BASIC FIBONACCI SERIES SOLVER WITH MEMOIZATION SOLUTION
+// This solution breaks up the code into two similarly-named functions with different signatures because it turns out, if you use the "inout" modifier in Swift to pass a dictionary by reference, you cannot have a default value for a non-supplied dictionary argument (as you start this call without a dictionary and it is normally instantiated as needed by the fib function itself in other languages). Mark any references to the dictionary with the address symbol "&" to indicate that a pointer to the one and only &memo object is moving into and out of the function. Also, the function declaration marks the argument with "inout" right before the type to show that it expects it to pass into and back out of the function by reference. Then, when the fibByReference() function is called with a single Int to start, the function that matches that signature will be called the first time, a memo dictionary will be generated so that when the 2nd call to this function is made, it will include the necessary 2nd argument -- a memo dictionary. After that point, all subsequent calls will use the function with 2 arguments because the memo dictionary will continue to exist between recursive calls of the 2nd function.
+
+func fibMemoByReference (_ n: Int) -> Int { // function having a signature with only 1 Int argument
+    var memo =  [Int:Int]()
+    return fibMemoByReference(n, &memo)
+}
+
+func fibMemoByReference (_ n: Int, _ memo: inout [Int:Int]) -> Int { // same function name, different signature, with both arguments, the original Int plus the memo dictionary generated from the first function.
+    if (memo.keys.contains(n)){ return memo[n]! }
+    if (n <= 2) {return 1}
+
+    memo[n] = fibMemoByReference(n - 1, &memo) + fibMemoByReference(n - 2, &memo)
+    return memo[n]!
+}
+
+print(fibMemoByReference(6))
+print(fibMemoByReference(7))
+print(fibMemoByReference(8))
+print(fibMemoByReference(50)) // Now runs as fast as the other 3, no problem!
+
+
+
+
+
